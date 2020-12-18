61bf7500f95b752b837e84691644b5b6ac2fe8e9
Can Narin <cannarin025@gmail.com>
2020-12-18 20:32:06 +0000

    moved all backend code to /Bin and created main.f.

--- Bin/Conways_Life.f
+++ Bin/Conways_Life.f
@@ -1,8 +1,8 @@
 \ : quit_sf 101719502 TYPE ;
 
 { array code}
-12 constant array_x_dim
-12 constant array_y_dim
+\ 200 constant array_x_dim          Array size defined in main.f
+\ 200 constant array_y_dim
 
 array_x_dim array_y_dim * constant array_size
 
@@ -27,7 +27,6 @@ create update_array array_size allot
 : update_array_@ array_x_dim * + update_array swap + C@ ; { takes input: x, y where x and y go from 0 - n-1}
 
 : update_array_! array_x_dim * + update_array swap + C! ; { takes input: value, x, y where x and y go from 0 - n-1}
-
 
 \ : reset_array
 \     array_size 0 do
@@ -60,18 +59,22 @@ create update_array array_size allot
 
 variable neighbor_sum   \ number of living neighbors a cell has
 variable alive_num      \ value a cell must have to be considered alive
+variable total_alive
+variable total_dead
+variable iteration
 0 neighbor_sum !
 1 alive_num !
+0 total_alive !
+0 total_dead !
+0 iteration !
 
-: check_neighbors   { (x,y) check_neighbors. Checks number of neigbors of cell at (x,y)} 
+: check_neighbors_unwrapped   { (x,y) check_neighbors. Checks number of neigbors of cell at (x,y)} 
     0 neighbor_sum ! \ resetting neighbor sum before use
     2 -1 do         \ y loop
         2 -1 do     \ x loop
             I 2 * J + 0=  \ ignores central "starting" point
-            if
-            \ ." pass" cr    
+            if    
             else
-                I . ." y " J . ." x "
                 { checking the neighbor is in grid}
                 swap dup J + 0 >= 
                 swap dup J + array_x_dim 1 - <= 
@@ -84,20 +87,63 @@ variable alive_num      \ value a cell must have to be considered alive
                 if 
                     swap dup rot dup rot swap   \ copies start coords for later use
                     swap J + swap I +   \ gets indices of adjacent cells
-                    \ dup . swap dup . swap ." indices" cr \ displays indices
                     array_@             \ gets value of adjacent cells. (checks cell at (x+n, y+n) for n = -1,0,1)
                     dup alive_num @ - 0=            \ checks number in cell is equal to alive num to check if adjacent cell is living
-                    if
-                        \ dup .    
+                    if 
                         neighbor_sum @ + neighbor_sum ! \ adds value to neighbor_sum
-                        \ ." value equal to alive_num" cr cr
-                        ." valid neighbor" cr
                     else
-                        ." value not equal to alive_num" cr
                         drop
                     then
                 else
-                   ." not in grid" cr cr
+                then
+            then
+        loop      \ ensures that n=0 is skipped to avoid checking "current" cell
+    loop  
+    drop drop        \ ensures that n=0 is skipped to avoid checking "current" cell
+    \ neighbor_sum @   \ puts neighbor_sum (number of surrounding alive cells) on stack
+;
+
+: check_neighbors_wrapped   { (x,y) check_neighbors. Checks number of neigbors of cell at (x,y)} 
+    0 neighbor_sum ! \ resetting neighbor sum before use
+    2 -1 do         \ y loop
+        2 -1 do     \ x loop
+            I 2 * J + 0=  \ ignores central "starting" point
+            if    
+            else 
+                \ I . ." I " J . ." J" cr
+                swap dup rot dup rot swap   \ copies start coords for later use
+                swap J + swap I +   \ gets indices of adjacent cells
+                
+                { wrapping edges}
+                dup array_y_dim - 0=
+                if  
+                    drop
+                    0
+                then
+                dup 1 + 0=
+                if
+                    drop
+                    array_y_dim 1 -
+                then swap
+                dup array_x_dim - 0=
+                if  
+                    drop
+                    0
+                then
+                dup 1 + 0=
+                if
+                    drop
+                    array_x_dim 1 -
+                then swap
+
+                \ swap dup rot dup rot . ." checking x   " . ." checking y" cr
+
+                array_@             \ gets value of adjacent cells. (checks cell at (x+n, y+n) for n = -1,0,1)
+                dup alive_num @ - 0=            \ checks number in cell is equal to alive num to check if adjacent cell is living
+                if 
+                    neighbor_sum @ + neighbor_sum ! \ adds value to neighbor_sum
+                else
+                    drop
                 then
             then
         loop      \ ensures that n=0 is skipped to avoid checking "current" cell
@@ -107,46 +153,45 @@ variable alive_num      \ value a cell must have to be considered alive
 ;
 
 : apply_rule { applies rules on cell (x,y) using value of neighbor_sum}
-    \ swap dup . ." x " swap dup . ." y" cr
     neighbor_sum @
     case
-        0 of 0 rot rot update_array_! show_update_array cr cr endof { takes x,y,value --> rotates to value x,y and moves to update_array}
-        1 of 0 rot rot update_array_! show_update_array cr cr endof
+        0 of 0 rot rot update_array_! endof { takes x,y,value --> rotates to value x,y and moves to update_array}
+        1 of 0 rot rot update_array_! endof
         2 of 
             swap dup rot dup rot swap array_@ alive_num @ - 0= 
             if 
                 alive_num @ rot rot update_array_!
+                total_alive @ 1 + total_alive !
             else
                 0 rot rot update_array_!
             then
         endof
-        3 of 1 rot rot update_array_! show_update_array cr cr endof
-        4 of 0 rot rot update_array_! show_update_array cr cr endof
-        5 of 0 rot rot update_array_! show_update_array cr cr endof
-        6 of 0 rot rot update_array_! show_update_array cr cr endof
-        7 of 0 rot rot update_array_! show_update_array cr cr endof
-        8 of 0 rot rot update_array_! show_update_array cr cr endof
-        \ drop DUP OF ."  no living neighbors found" endof
+        3 of alive_num @ rot rot update_array_! total_alive @ 1 + total_alive ! endof
+        4 of 0 rot rot update_array_! endof
+        5 of 0 rot rot update_array_! endof
+        6 of 0 rot rot update_array_! endof
+        7 of 0 rot rot update_array_! endof
+        8 of 0 rot rot update_array_! endof
     endcase
-    neighbor_sum @ . ." living neighbors found"
-    cr cr
+
+    array_size total_alive @ - total_dead !
+
 ;
 
-: update_game 
+: update_game_unwrapped 
     { updates update_array}
 
     reset_update_array
 
+    0 total_alive !
+    0 total_dead !
+
     array_y_dim 0 do
         array_x_dim 0 do
-            I . ." y " J . ." x" cr
-            J I check_neighbors 
+            J I check_neighbors_unwrapped 
             J I apply_rule
-            cr cr cr cr cr cr cr 
         loop
     loop
-
-    show_update_array
 
     { reads update array and updates conway_array}
     array_y_dim 0 do
@@ -155,38 +200,38 @@ variable alive_num      \ value a cell must have to be considered alive
             I J array_!
         loop
     loop
+
+    iteration @ 1 + iteration !
 ;
 
-{ shapes}
-: boat \ still life with bottom left corner at x,y
-1 1 pick 1 + 1 pick array_!
-1 1 pick 1 pick 1 + array_!
-1 1 pick 1 pick 2 + array_!
-1 1 pick 2 + 1 pick 1 + array_!
-1 1 pick 1 + 1 pick 2 + array_!
-drop drop 
+: update_game_wrapped
+    { updates update_array}
+
+    0 total_alive !
+    0 total_dead !
+
+    reset_update_array
+
+    array_y_dim 0 do
+        array_x_dim 0 do
+            J I check_neighbors_wrapped
+            J I apply_rule
+        loop
+    loop
+
+    { reads update array and updates conway_array}
+    array_y_dim 0 do
+        array_x_dim 0 do
+            I J update_array_@
+            I J array_!
+        loop
+    loop
+
+    iteration @ 1 + iteration !
+    
 ;
 
-: lwss \ lightweight spaceship, bottom left corner @ x,y
-1 1 pick 1 pick array_!
-1 1 pick 3 + 1 pick array_!
-1 1 pick 1 pick 2 + array_!
-1 1 pick 1 + 1 pick 3 + array_!
-1 1 pick 2 + 1 pick 3 + array_!
-1 1 pick 3 + 1 pick 3 + array_!
-1 1 pick 4 + 1 pick 3 + array_!
-1 1 pick 4 + 1 pick 2 + array_!
-1 1 pick 4 + 1 pick 1 + array_!
-drop drop 
-; 
+: live 
+    alive_num @ rot rot array_!
+;
 
-\ reset_array
-\ show_array
-\ \ 1 1 1 array_!
-\ \ 1 1 2 array_!
-\ \ 1 2 1 array_!
-\ \ 1 2 2 array_!
-\ 1 1 boat
-\ show_array
-\ update_game
-\ show_array
---
+++ Main.f
@@ -0,0 +1,12 @@
+{ Array parameters}
+
+200 constant array_x_dim
+200 constant array_y_dim
+
+Include Bin/Conways_Life.f
+Include Bin/Lifes.f
+Include Bin/Text_Output_Window_V1.f
+Include Bin/Test_File_IO_Tools.f
+Include Bin/Graphics_V6_Single_Scaled_BMP_Window.f
+
+init_life
